610224962c711992242e9824f8f706854724fa42
danny avendaño <made.loprios@gmail.com>
2025-02-06 17:45:19 -0500

    Se añade aplicación web MVC con la plantilla inicial

---
+++ ModelosEvaluativos/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ModelosEvaluativos.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
