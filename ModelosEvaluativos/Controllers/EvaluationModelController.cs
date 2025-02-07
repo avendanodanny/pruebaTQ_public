3d4320e1c1045510a4a5625cfb3af818c5d663b9
danny avendaño <made.loprios@gmail.com>
2025-02-07 08:22:34 -0500

    Se deja funcionando el botón de crear nuevo modelo evaluativo

--- ModelosEvaluativos/Controllers/EvaluationModelController.cs
+++ ModelosEvaluativos/Controllers/EvaluationModelController.cs
@@ -18,4 +18,33 @@ public class EvaluationModelController : Controller
     {
         return View(await _context.Models.ToListAsync());
     }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult CreateModel([Bind("nombre,descripcion,estado")] EvaluationModel model)
+    {
+        
+        try
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(model);
+                _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+        }
+        catch (DbUpdateException /* ex */)
+        {
+            //Log the error (uncomment ex variable name and write a log.
+            ModelState.AddModelError("", "Unable to save changes. " +
+                "Try again, and if the problem persists " +
+                "see your system administrator.");
+        }
+        return View(model);
+    }
+
 }
